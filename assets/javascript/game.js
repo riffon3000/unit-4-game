13ef2d2b51ffd37cf7b01ef2a5d4f6bd7fd92653
riffon3000 <riffon@gmail.com>
2018-09-09 20:55:36 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,4 +3,31 @@ var wins = 0;
 var losses = 0;
 var score = 0;
 
-// initialize variables that reference location in html
+// function to generate and return random numbers with range between 19 and 120
+var randomNumber = random(19, 120);
+var randomPoint = random(1, 12);
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+console.log(randomNumber);
+console.log(randomPoint);
+
+// if player score is equal to randomPoint add +1 to wins
+if (score === randomNumber) {
+    ++wins;
+}
+
+// if player score is more than randomPoint add +1 to losses
+if (score >= randomNumber) {
+    ++losses;
+}
+
+// return variables to referenced location in html
+$("#random-number").text(randomNumber);
+console.log(wins);
+$("#number-wins").text("Wins: "+ wins);
+console.log(losses);
+$("#number-losses").text("Losses: " + losses);
+console.log(score);
+$("#your-score").text(score);
